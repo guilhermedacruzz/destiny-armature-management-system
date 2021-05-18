b232d165f7fe3f4115dbb630de4a8cee2b8ca859
indexGuilherme <guidacruz2004@gmail.com>
2021-05-18 16:24:26 -0300

    Ajuste de Estilo na Tela de Escolha de Personagem

--- src/info/oo/control/CharacterScene.java
+++ src/info/oo/control/CharacterScene.java
@@ -1,5 +1,6 @@
 package info.oo.control;
 
+import info.oo.Main;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -7,7 +8,6 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-import java.beans.EventHandler;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -24,9 +24,13 @@ public class CharacterScene implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        btHunter.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/assets/images/hunter2.png"))));
-        btWarlock.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/assets/images/warlock2.png"))));
-        btTitan.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/assets/images/titan2.png"))));
+        btHunter.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/assets/images/hunter_logo.png"))));
+        btWarlock.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/assets/images/warlock_logo.png"))));
+        btTitan.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/assets/images/titan_logo.png"))));
+    }
 
+    @FXML
+    void comeBack() {
+        Main.changeSceneFade(Main.LOGIN, (aClass)-> new LoginScene());
     }
 }
