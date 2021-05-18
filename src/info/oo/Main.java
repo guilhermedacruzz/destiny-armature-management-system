860ec870fb06bcca58d36d92b1c27b1d5fad9be7
indexGuilherme <guidacruz2004@gmail.com>
2021-05-18 11:02:54 -0300

    Testando animações entre as telas

--- src/info/oo/Main.java
+++ src/info/oo/Main.java
@@ -2,6 +2,10 @@ package info.oo;
 
 import info.oo.control.LoginScene;
 import info.oo.control.SignInScene;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,6 +13,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.util.Callback;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -16,7 +21,7 @@ public class Main extends Application {
 
     public static final String LOGIN = "/fxml/LoginScene.fxml";
     public static final String CADASTRO = "/fxml/SignInScene.fxml";
-    private static StackPane base;
+    private static StackPane stackPane;
 
     public static void main(String[] args) {
         launch(args);
@@ -25,12 +30,60 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        base = new StackPane();
-        primaryStage.setScene(new Scene(base, 801, 534));
+        stackPane = new StackPane();
+        primaryStage.setScene(new Scene(stackPane, 801, 534));
 
         mudaCena(Main.LOGIN,(aClass)->new LoginScene());
 
         primaryStage.show();
+    }
+
+    public static void mudaCenaDireita(String cena, Callback construtor) {
+        try{
+            var paneToRemove = stackPane.getChildren().get(0);
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource(cena));
+            loader.setControllerFactory(construtor);
+            Parent paneToAdd = loader.load();
+
+            paneToAdd.translateXProperty().set(stackPane.getWidth());
+            stackPane.getChildren().add(paneToAdd);
+
+            var keyValue = new KeyValue(paneToAdd.translateXProperty(), 0, Interpolator.EASE_IN);
+            var keyFrame = new KeyFrame(Duration.millis(900), keyValue);
+            var timeline = new Timeline(keyFrame);
+            timeline.setOnFinished(evt -> {
+                stackPane.getChildren().remove(paneToRemove);
+            });
+            timeline.play();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void mudaCenaEsquerda(String cena, Callback construtor) {
+        try{
+            var paneToRemove = stackPane.getChildren().get(0);
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource(cena));
+            loader.setControllerFactory(construtor);
+            Parent paneToAdd = loader.load();
+
+            paneToAdd.translateXProperty().set(-1 * stackPane.getWidth());
+            stackPane.getChildren().add(paneToAdd);
+
+            var keyValue = new KeyValue(paneToAdd.translateXProperty(), 0, Interpolator.EASE_IN);
+            var keyFrame = new KeyFrame(Duration.millis(900), keyValue);
+            var timeline = new Timeline(keyFrame);
+            timeline.setOnFinished(evt -> {
+                stackPane.getChildren().remove(paneToRemove);
+            });
+            timeline.play();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     public static void mudaCena(String cena, Callback construtor){
@@ -39,12 +92,12 @@ public class Main extends Application {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(Main.class.getResource(cena));
             loader.setControllerFactory(construtor);
-            Parent root = loader.load();
+            Parent paneToAdd = loader.load();
 
-            if (base.getChildren().stream().count() > 0) {
-                base.getChildren().remove(0);
+            if (stackPane.getChildren().stream().count() > 0) {
+                stackPane.getChildren().remove(0);
             }
-            base.getChildren().add(root);
+            stackPane.getChildren().add(paneToAdd);
 
         }catch (IOException e){
             e.printStackTrace();
