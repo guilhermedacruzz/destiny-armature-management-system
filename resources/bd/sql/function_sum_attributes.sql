7093be2fb36cbf4f616ebead78fe638d00f636e8
indexGuilherme <guidacruz2004@gmail.com>
2021-05-23 11:03:50 -0300

    Implementando a função que soma os atributos das armaduras.

---
+++ resources/bd/sql/function_sum_attributes.sql
@@ -0,0 +1,21 @@
+use sgda1;
+
+delimiter $$
+drop function if exists sum_attributes $$
+create function sum_attributes (index_attribute int)
+returns INT
+deterministic 
+begin
+	declare mob, res, rec, dic, inte, stre int;
+    
+    select mobility, resilience, recovery, dicipline, intellect, strenght
+    into mob, res, rec, dic, inte, stre
+    from table_attributes where cod_attributes = index_attribute;
+    
+	return mob + res + rec + dic + inte + stre;
+end $$
+delimiter ;
+
+select * from table_attributes;
+select sum_attributes(1);
+
