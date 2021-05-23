494317efc265beaf8260d94c3d4642fb2daf76a1
indexGuilherme <guidacruz2004@gmail.com>
2021-05-23 12:27:44 -0300

    Implementando a procedure que busca das armaduras por categoria e raridade.

---
+++ resources/bd/sql/procedure_type_rarity_armor.sql
@@ -0,0 +1,19 @@
+use sgda1;
+
+delimiter $$
+drop procedure if exists rarity_type_armor $$
+create procedure rarity_type_armor(IN type_armor VARCHAR(25), IN rarity_armor VARCHAR(25))
+begin
+	select a.name, a2.mobility, a2.resilience, a2.recovery, a2.dicipline, a2.intellect, a2.strenght
+    from table_armor a , table_armor_attributes a1, table_attributes a2
+    where a.cod_armor=a1.cod_armor and a1.cod_attributes=a2.cod_attributes
+    and a.type=type_armor and a.rarity=rarity_armor;
+end $$
+delimiter ;
+
+select * from table_armor;
+#select * from table_attributes;
+#insert into table_armor_attributes(cod_armor, cod_attributes) values(2, 1);
+#select * from table_armor_attributes;
+
+call rarity_type_armor("b", "d");
