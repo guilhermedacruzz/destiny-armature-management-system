77eb5410e8d6da0d03505a7a03838598b102eeb6
indexGuilherme <guidacruz2004@gmail.com>
2021-05-23 12:24:38 -0300

    Implementando a procedure que busca das armaduras por categoria.

--- resources/bd/sql/table_armor_attributes.sql
+++ resources/bd/sql/table_armor_attributes.sql
@@ -1,9 +1,9 @@
 use sgda1;
 
 drop table if exists table_armor_attributes;
-create table table_armor_attributtes(
+create table table_armor_attributes(
 	cod_armor INT not null,
-    cod_attribuites INT not null,
+    cod_attributes INT not null,
     
     FOREIGN KEY(cod_armor) references table_armor(cod_armor),
     FOREIGN KEY(cod_attributes) references table_attributes(cod_attributes)
