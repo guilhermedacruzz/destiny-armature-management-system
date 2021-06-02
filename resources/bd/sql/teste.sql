97d6f2d9144193cde7c2f320f80662acafb4581a
indexGuilherme <guidacruz2004@gmail.com>
2021-06-02 11:56:24 -0300

    Refatorando e Unificando o css dos Botões.

--- resources/bd/sql/teste.sql
+++ resources/bd/sql/teste.sql
@@ -1,2 +1,4 @@
-use sgda77;
-select * from table_armor where cod_user=1 and guardian_class="Arcano";
+use sgda78;
+
+update table_user set lumen=1, legendary_fragments=2, ascendent_fragments=3, enhancement_prism=4, improvement_core=5, enhancement_module=6 where cod_user=1;
+select * from table_user;
