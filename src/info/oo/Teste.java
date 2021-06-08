dcf183ce71eee1d371becc348a57a91a9913747a
indexGuilherme <guidacruz2004@gmail.com>
2021-06-08 09:42:32 -0300

    Tentativa de uma tela de Resultados.

---
+++ src/info/oo/Teste.java
@@ -0,0 +1,33 @@
+package info.oo;
+
+import info.oo.control.ViewArmorScene;
+import info.oo.control.ViewArmorSetScene;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Teste extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader();
+
+        loader.setLocation(Main.class.getResource("/fxml/ViewArmorSetScene.fxml"));
+        loader.setControllerFactory((aClass) -> new ViewArmorSetScene());
+
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root,801,534);
+
+        stage.setScene(scene);
+        stage.setTitle("Cadastro Animais de Estimação");
+        stage.show();
+    }
+}
