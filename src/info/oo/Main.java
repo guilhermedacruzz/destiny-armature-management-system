c040ed3451234347303eb04c45e6ea7f7dfe2b84
indexGuilherme <guidacruz2004@gmail.com>
2021-06-15 10:53:07 -0300

    Atualizando o relatório semanal

--- src/info/oo/Main.java
+++ src/info/oo/Main.java
@@ -60,7 +60,7 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
 
         initDAOS();
         initRepository();
